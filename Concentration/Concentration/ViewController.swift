bf04ee49b9b808e5f548074be4da4bc72afe91ac
KakaoTocs <kimjinu5555@gmail.com>
2018-10-14 18:34:21 +0900

    Computed Property, AccessControl, Extension, Enum, Optional, StackView
    Computed Property: 연산 프로퍼티를 사용하면 좋은 상황
    AccessControl: 접근제어 설정 상황
    Extension: 진짜 올바른 사용 예시
    Enum, Optional: 옵셔널내부가 Enum으로 이루어져있고, 값을 가져올때 내부 처리 방식(switch, chaning->순서로 인한 장점)
    StackView: Use standard value

--- Concentration/Concentration/ViewController.swift
+++ Concentration/Concentration/ViewController.swift
@@ -9,21 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-    lazy var game: Concentration = Concentration(numberOfPairsOfCards: (cardButtons.count + 1) / 2)
-    var flipCount = 0 {
+    private lazy var game: Concentration = Concentration(numberOfPairsOfCards: numberOfPairsOfCards)
+    var numberOfPairsOfCards: Int {
+        return (cardButtons.count + 1) / 2
+    }
+    private(set) var flipCount = 0 {
         didSet {
             flipCountLabel.text = "Flips: \(flipCount)"
         }
     }
     
-    @IBOutlet weak var flipCountLabel: UILabel!
-    @IBOutlet var cardButtons: [UIButton]!
+    @IBOutlet private weak var flipCountLabel: UILabel!
+    @IBOutlet private var cardButtons: [UIButton]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-    @IBAction func touchCard(_ sender: UIButton) {
+    @IBAction private func touchCard(_ sender: UIButton) {
         flipCount += 1
         if let cardNumber = cardButtons.lastIndex(of: sender) {
             game.chooseCard(at: cardNumber)
@@ -33,7 +36,7 @@ class ViewController: UIViewController {
         }
     }
     
-    func updateViewFromModel() {
+    private func updateViewFromModel() {
         for index in cardButtons.indices {
             let button = cardButtons[index]
             let card = game.cards[index]
@@ -47,15 +50,25 @@ class ViewController: UIViewController {
         }
     }
     
-    var emojichoices = ["🦇", "😱", "🙀", "😈", "🎃", "👻", "🍭", "🍬", "🍎"]
-    var emoji = [Int:String]()
-    func emoji(for card: Card) -> String {
+    private var emojichoices = ["🦇", "😱", "🙀", "😈", "🎃", "👻", "🍭", "🍬", "🍎"]
+    private var emoji = [Int:String]()
+    private func emoji(for card: Card) -> String {
         if emoji[card.identifier] == nil, emojichoices.count > 0 {
-            let randomIndex = Int(arc4random_uniform(UInt32(emojichoices.count)))
-            emoji[card.identifier] = emojichoices.remove(at: randomIndex)
+            emoji[card.identifier] = emojichoices.remove(at: emojichoices.count.arc4random)
         }
         return emoji[card.identifier] ?? "?"
     }
     
 }
 
+extension Int {
+    var arc4random: Int {
+        if self > 0 {
+            return Int(arc4random_uniform(UInt32(self)))
+        } else if self < 0 {
+            return -Int(arc4random_uniform(UInt32(abs(self))))
+        } else {
+            return 0
+        }
+    }
+}
